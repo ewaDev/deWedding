52d6e482fd9e3984f6d3b14be6142d093ad757c8
TheMicroGirl <e.grabowiecka@outlook.com>
2019-09-01 13:35:15 +0100

    init+paging

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navbar from './partials/navbar';
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ layouts/partials/navbar.vue
@@ -0,0 +1,26 @@
+<template>
+<header class="header">
+    
+    <nuxt-link to="/" class="logo">Home</nuxt-link>
+     <input class="menu-btn" type="checkbox" id="menu-btn" />
+     <label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+    <nav>
+        <ul class="menu">
+            <li><nuxt-link to="/aboutus" class="navlink">AboutUs</nuxt-link></li>
+            <li><nuxt-link to="/rsvp" class="navlink">RSVP</nuxt-link></li>
+            <li><nuxt-link to="/venue" class="navlink">Venue</nuxt-link></li>
+        </ul>
+    </nav>
+</header>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style >
+
+</style>
