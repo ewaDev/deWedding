2f2cc242996a47d14e3b77112e6ea8a917a3a200
TheMicroGirl <e.grabowiecka@outlook.com>
2019-09-01 17:25:23 +0100

    new form

--- pages/rsvp.vue
+++ pages/rsvp.vue
@@ -1,67 +1,40 @@
 <template>
   <div class="container">
-    <div>
-      <h1>{{ teaName }}</h1>
-    </div>
-    <div>
-      <form
-        name="vue-tea"
-        method="post"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="form-name" value="vue-tea" />
-        <div class="tea">
-          <h2>Pick a Tea</h2>
-          <label
-            v-for="tea in teaNames"
-            :key="tea"
-            :class="{
-              'tea-label': true,
-              checked: tea === chosenTea
-            }"
-          >
-            <input
-              id="tea"
-              v-model="chosenTea"
-              name="tea"
-              type="radio"
-              :value="tea"
-            />
-            <span>{{ tea }}</span>
-          </label>
-        </div>
+  
+<form name="contact" method="POST" data-netlify="true">
+  <p>
+    <p><label>Your Name: <input name="name" type="text" v-model="name"></label> {{name}}
+    <p><label>Your Email: <input name="email" type="text" v-model="email"></label>{{email}}
+   
+  </p>
+
+
+
+
+
+
+
+
+
+
+  
+    
+      
         <button>Submit</button>
       </form>
     </div>
-  </div>
+
 </template>
 
 <script>
 export default {
   data() {
     return {
-      chosenTea: 'Earl Grey',
-      teaTypes: {
-        'Earl Grey': 'London',
-        'Irish Breakfast': 'Dublin',
-        Chai: 'Bombay',
-        Rose: 'Atlantic City',
-        Matcha: 'Tokyo',
-        Rooibos: 'Cape Town',
-        'Yerba Mate': 'Montreal',
-        Green: 'Oregon Mist'
-      }
-    }
-  },
-  computed: {
-    teaName() {
-      return `${this.teaTypes[this.chosenTea]} Fog`
-    },
-    teaNames() {
-      return Object.keys(this.teaTypes)
+      name: null,
+      email: null
     }
   }
+  
 }
 </script>
 
