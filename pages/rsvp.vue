887093710e4cb9293073641a5c83c8c14fb289a4
TheMicroGirl <e.grabowiecka@outlook.com>
2019-09-01 16:44:33 +0100

    new form

--- pages/rsvp.vue
+++ pages/rsvp.vue
@@ -1,29 +1,68 @@
 <template>
   <div class="container">
-    <form name="rsvp" method="POST" data-netlify="true">
-    <p>
-        <label>Your Name: <input type="text" name="name" /></label>   
-    </p>
-    <p>
-        <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-        <label>Your PhoneNumber: <input type="phone" name="phone" /></label>
-    </p>
-    <p>
-        <label>Message: <textarea name="message"></textarea></label>
-    </p>
-    <p>
-      <div data-netlify-recaptcha="true"></div>
-    </p>
-    <p>
-        <button type="submit" value="submit">Send</button>
-    </p>
-    </form>  
+    <div>
+      <h1>{{ teaName }}</h1>
+    </div>
+    <div>
+      <form
+        name="vue-tea"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="vue-tea" />
+        <div class="tea">
+          <h2>Pick a Tea</h2>
+          <label
+            v-for="tea in teaNames"
+            :key="tea"
+            :class="{
+              'tea-label': true,
+              checked: tea === chosenTea
+            }"
+          >
+            <input
+              id="tea"
+              v-model="chosenTea"
+              name="tea"
+              type="radio"
+              :value="tea"
+            />
+            <span>{{ tea }}</span>
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+export default {
+  data() {
+    return {
+      chosenTea: 'Earl Grey',
+      teaTypes: {
+        'Earl Grey': 'London',
+        'Irish Breakfast': 'Dublin',
+        Chai: 'Bombay',
+        Rose: 'Atlantic City',
+        Matcha: 'Tokyo',
+        Rooibos: 'Cape Town',
+        'Yerba Mate': 'Montreal',
+        Green: 'Oregon Mist'
+      }
+    }
+  },
+  computed: {
+    teaName() {
+      return `${this.teaTypes[this.chosenTea]} Fog`
+    },
+    teaNames() {
+      return Object.keys(this.teaTypes)
+    }
+  }
+}
 </script>
 
 <style>
@@ -31,7 +70,9 @@
   margin: 0 auto;
   min-height: 100vh;
   display: flex;
-
+  justify-content: center;
+  align-items: center;
+  text-align: center;
 }
 
 .title {
